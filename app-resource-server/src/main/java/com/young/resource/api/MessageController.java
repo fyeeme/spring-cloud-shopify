00dffb2283e3e7ee5f44ab7d3c0c66fe5d7db11f
LiuYang <fyeeme@gmail.com>
2023-07-09 18:53:58 +0800

    feat: custom JwtAuthenticationConverter

--- app-resource-server/src/main/java/com/young/resource/api/MessageController.java
+++ app-resource-server/src/main/java/com/young/resource/api/MessageController.java
@@ -1,5 +1,8 @@
 package com.young.resource.api;
 
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.context.annotation.Role;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
     @GetMapping("/messages")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    //@RolesAllowed("SCOPE_message.read")
     public String[] getMessages() {
         return new String[]{"Message 1", "Message 2", "Message 3"};
     }
--- app-resource-server/src/main/java/com/young/resource/config/ResourceServerConfig.java
+++ app-resource-server/src/main/java/com/young/resource/config/ResourceServerConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -11,6 +13,8 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -18,18 +22,33 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @EnableWebSecurity
 @Configuration(proxyBeanMethods = false)
+@EnableMethodSecurity(jsr250Enabled = true, securedEnabled = true)
 public class ResourceServerConfig {
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuerUri;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.securityMatcher("/messages/**")
-                .authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests.requestMatchers("/messages/**").hasAuthority("SCOPE_message.read"))
+        http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2ResourceServer ->
                         oauth2ResourceServer.jwt(Customizer.withDefaults()));
+        //http.securityMatcher("/messages/**")
+        //        .authorizeHttpRequests(authorizeHttpRequests ->
+        //                authorizeHttpRequests.requestMatchers("/messages/**").hasAuthority("SCOPE_message.read"))
+        //        .oauth2ResourceServer(oauth2ResourceServer ->
+        //                oauth2ResourceServer.jwt(Customizer.withDefaults()));
         return http.build();
+    }
+
+    @Bean
+    JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
     }
 
     @Bean
