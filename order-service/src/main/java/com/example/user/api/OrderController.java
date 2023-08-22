1eac0206301c3ff82215d55f1bae9e84007729b5
liuyangyang <fyeeme@gmail.com>
2023-08-22 22:29:08 +0800

    chore: update nacos config

--- order-service/src/main/java/com/example/user/api/OrderController.java
+++ order-service/src/main/java/com/example/user/api/OrderController.java
@@ -1,22 +1,25 @@
 package com.example.user.api;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class OrderController {
     private final UserApi userApi;
 
-    public OrderController(UserApi userApi) {
-        this.userApi = userApi;
-    }
+    @Value("${spring.application.name}")
+    private String name;
 
     @GetMapping("/echo/{string}")
     public String echo(@PathVariable String string) {
         String userMsg = userApi.echo(string);
-        return "hello Nacos Discovery " + string + ",msg from user api: " + userMsg;
+        return "hello Nacos Discovery " + string + name + ",msg from user api: " + userMsg;
     }
 
     @GetMapping("/divide")
